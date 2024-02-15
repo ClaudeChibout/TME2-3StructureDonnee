c7ccb6022f5afdf1b051c86a4dd4c0ad634c4a2c
deryyaa <derya-yaya@outlook.fr>
2024-02-15 12:03:11 +0100

    pull

--- exo1/biblioC.c
+++ exo1/biblioC.c
@@ -50,3 +50,64 @@ void inserer_en_tete(Biblio* b, int num, char * titre, char * auteur){
         b->L->suiv=livre;
     }
 }
+
+void affichage_livre(Livre * l){
+    if (l!=NULL){
+        printf("num : %d, titre: %s, auteur: %s \n",l->num,l->titre,l->auteur);
+           
+        }
+    }
+}
+
+void affichage_bibliotheque(Biblio * b){
+    Livre * livre = b->L;
+    if(b!=NULL){
+        while(b->L){
+            affichage_livre(b->L);
+            b->L=b->L->suiv;
+        }
+        b->L=livre;
+    }
+}
+
+Livre* recherche_ouvrage_num(Biblio * b, int num){
+    Livre * l=b->L;
+    if(b!=NULL){
+        while(l){
+            if(l->num=num){
+                return l;
+            }
+            l=l->suiv;
+        }
+    }
+}
+
+
+Livre* recherche_ouvrage_titre(Biblio * b, char * titre){
+    Livre * l=b->L;
+    if(b!=NULL){
+        while(l){
+            if(strcmp(l->titre,titre)){
+                return l;
+            }
+            l=l->suiv;
+        }
+    }
+}
+
+Biblio* recherche_livres_auteur(Biblio * b, char * auteur){
+    Livre * l=b->L;
+    Biblio * bnew=creer_biblio();
+    if(b!=NULL){
+        while(l){
+            if (strcmp(l->auteur,auteur)){
+                inserer_en_tete(bnew, l->num, l->titre, l->auteur);
+            }
+            l=l->suiv;
+        }
+    }
+    return bnew;
+}
+
+
+
