c4fd6fc82cff75cf88363cd59274e2eb64492e83
deryyaa <derya-yaya@outlook.fr>
2024-02-18 18:30:27 +0100

    ss

--- exo1/biblioC.c
+++ exo1/biblioC.c
@@ -173,7 +173,7 @@ void fusion_deux_bibliotheque(Biblio ** b1, Biblio ** b2){
         livre_temp=livre_tete1;
         int cmp=0;
         while(livre_tete){
-            if(strcmp(livre_tete->auteur,livre_temp->auteur)==0 && strcmp(livre_tete->titre,livre_temp->titre)==0){
+            if(strcmp(livre_tete->auteur,livre_temp->auteur)==0 && strcmp(livre_tete->titre,livre_temp->titre)==0 && livre_temp->num != livre_tete->num){
                 Livre * tmp=creer_livre(livre_tete->num, livre_tete->titre, livre_tete->auteur);
                 tmp->suiv=livre_double;
                 livre_double=tmp;
